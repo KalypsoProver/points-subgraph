bd6acef0626eea58f18e976653efae910e680a77
Prateek <prateekreddyy@gmail.com>
2025-02-27 15:22:08 +0400

    feat: Points breakdown by epoch and operator

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { BigInt, log, TypedMap } from '@graphprotocol/graph-ts';
-import { Delegation, EpochState, Generator, GeneratorMarketInfo, GlobalState, JobsPerEpoch, TotalDelegation, TotalJobsPerEpoch, User } from '../generated/schema';
+import { Delegation, EpochState, Generator, GeneratorMarketInfo, GlobalState, JobsPerEpoch, PointsByEpoch, PointsByGenerator, TotalDelegation, TotalJobsPerEpoch, User } from '../generated/schema';
 
 import { E18, GLOBAL_STATE_ID } from './constants';
 
@@ -75,6 +75,11 @@ function distributePointsForEpoch(epoch: BigInt): void {
         log.warning('Epoch state for {} not found when distributing rewards', [epoch.toString()]);
         return;
     }
+    
+    // Track points by epoch for each user
+    let pointsTrackingByAddress = new TypedMap<string, BigInt>();
+    let pointsTrackingByGeneratorAndUser = new TypedMap<string, BigInt>();
+    
     for(let i=0; i < generatorList.length; i++) {
         const generator = generatorList[i];
         let totalGeneratorJobsInEpoch = BigInt.fromI32(0);
@@ -110,6 +115,19 @@ function distributePointsForEpoch(epoch: BigInt): void {
         const generatorShare = reward.times(commission).div(E18);
         generatorUserEntity.points = generatorUserEntity.points.plus(generatorShare);
         generatorUserEntity.save();
+        
+        // Track generator's points for this epoch
+        let existingPoints = pointsTrackingByAddress.get(generator);
+        let newPoints = existingPoints ? existingPoints.plus(generatorShare) : generatorShare;
+        pointsTrackingByAddress.set(generator, newPoints);
+        
+        // TODO: Self generation of points for generator isn't considered
+        // Track this specific generator's points as its own source (self-generation)
+        let generatorSelfKey = generator + '-' + generator;
+        let currentGeneratorSelfPoints = pointsTrackingByGeneratorAndUser.get(generatorSelfKey);
+        let updatedGeneratorSelfPoints = currentGeneratorSelfPoints ? currentGeneratorSelfPoints.plus(generatorShare) : generatorShare;
+        pointsTrackingByGeneratorAndUser.set(generatorSelfKey, updatedGeneratorSelfPoints);
+        
         reward = reward.minus(generatorShare);
 
         let rewardsPerToken = new TypedMap<string, BigInt>();
@@ -156,6 +174,64 @@ function distributePointsForEpoch(epoch: BigInt): void {
             }
             user.points = user.points.plus(userShare);
             user.save();
+            
+            // Track delegator's points for this epoch
+            let currentDelegatorPoints = pointsTrackingByAddress.get(delegation.delegator);
+            let updatedDelegatorPoints = currentDelegatorPoints ? currentDelegatorPoints.plus(userShare) : userShare;
+            pointsTrackingByAddress.set(delegation.delegator, updatedDelegatorPoints);
+            
+            // Track points by generator for this delegator
+            let delegatorGeneratorKey = delegation.delegator + '-' + generator;
+            let currentDelegatorGeneratorPoints = pointsTrackingByGeneratorAndUser.get(delegatorGeneratorKey);
+            let updatedDelegatorGeneratorPoints = currentDelegatorGeneratorPoints ? currentDelegatorGeneratorPoints.plus(userShare) : userShare;
+            pointsTrackingByGeneratorAndUser.set(delegatorGeneratorKey, updatedDelegatorGeneratorPoints);
+        }
+    }
+    
+    // Save accumulated points by epoch
+    for (let i = 0; i < pointsTrackingByAddress.entries.length; i++) {
+        const address = pointsTrackingByAddress.entries[i].key;
+        const points = pointsTrackingByAddress.entries[i].value;
+        
+        let pointsByEpoch = PointsByEpoch.load(epoch.toString() + '-' + address);
+        if (pointsByEpoch == null) {
+            pointsByEpoch = new PointsByEpoch(epoch.toString() + '-' + address);
+            pointsByEpoch.address = address;
+            pointsByEpoch.epoch = epoch;
+            pointsByEpoch.points = BigInt.fromI32(0);
+            pointsByEpoch.pointsByGenerator = [];
+        }
+        
+        pointsByEpoch.points = pointsByEpoch.points.plus(points);
+        pointsByEpoch.save();
+    }
+    
+    // Save points by generator
+    for (let i = 0; i < pointsTrackingByGeneratorAndUser.entries.length; i++) {
+        const key = pointsTrackingByGeneratorAndUser.entries[i].key;
+        const points = pointsTrackingByGeneratorAndUser.entries[i].value;
+        
+        const keyParts = key.split('-');
+        const address = keyParts[0];
+        const generator = keyParts[1];
+        
+        let pointsByGenerator = PointsByGenerator.load(epoch.toString() + '-' + address + '-' + generator);
+        if (pointsByGenerator == null) {
+            pointsByGenerator = new PointsByGenerator(epoch.toString() + '-' + address + '-' + generator);
+            pointsByGenerator.address = address;
+            pointsByGenerator.epoch = epoch;
+            pointsByGenerator.generator = generator;
+            pointsByGenerator.points = BigInt.fromI32(0);
+        }
+        
+        pointsByGenerator.points = pointsByGenerator.points.plus(points);
+        pointsByGenerator.save();
+        
+        // Link to parent PointsByEpoch entity
+        let pointsByEpoch = PointsByEpoch.load(epoch.toString() + '-' + address);
+        if (pointsByEpoch != null) {
+            pointsByEpoch.pointsByGenerator = pointsByEpoch.pointsByGenerator.concat([pointsByGenerator.id]);
+            pointsByEpoch.save();
         }
     }
 }
